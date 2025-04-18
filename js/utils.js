b6d1b9edc2a5ea21dc280fbd3aac39819e2eb646
Zara <zaraseshadri@Zaras-MacBook-Air.local>
2025-04-18 00:50:06 -0300

    Trying to fix what I ruined :(

--- js/utils.js
+++ js/utils.js
@@ -6,130 +6,12 @@ are used in the runtime simulator.
 Author: Zara Phukan.
 Creation Date: April 15, 2025.
 */
-// custom enum for stating instruction format
-var OperandType;
-(function (OperandType) {
-    OperandType[OperandType["IMMEDIATE"] = 0] = "IMMEDIATE";
-    OperandType[OperandType["REGISTER"] = 1] = "REGISTER";
-})(OperandType || (OperandType = {}));
 /*** Constants ***/
 const registerDisplays = [];
 for (let i = 0; i < XLEN; i++) {
     registerDisplays.push(document.querySelector(`#register-${i}`));
 }
 registerDisplays.push(document.querySelector("#pc-register"));
-const I_TYPE = [
-    OperandType.REGISTER,
-    OperandType.REGISTER,
-    OperandType.IMMEDIATE,
-];
-const U_TYPE = [OperandType.REGISTER, OperandType.IMMEDIATE];
-const R_TYPE = [
-    OperandType.REGISTER,
-    OperandType.REGISTER,
-    OperandType.REGISTER,
-];
-const NONE_TYPE = [];
-const PSEUDO_TYPE = [OperandType.REGISTER, OperandType.REGISTER];
-const J_TYPE = [];
-// for storing the current values in the registers
-const registers = new Map(Array.from({ length: 33 }, (_, i) => [i, 0]));
-const STRINGS_TO_REGISTERS = new Map([
-    // General Register Names
-    ["x0", 0],
-    ["x1", 1],
-    ["x2", 2],
-    ["x3", 3],
-    ["x4", 4],
-    ["x5", 5],
-    ["x6", 6],
-    ["x7", 7],
-    ["x8", 8],
-    ["x9", 9],
-    ["x10", 10],
-    ["x11", 11],
-    ["x12", 12],
-    ["x13", 13],
-    ["x14", 14],
-    ["x15", 15],
-    ["x16", 16],
-    ["x17", 17],
-    ["x18", 18],
-    ["x19", 19],
-    ["x20", 20],
-    ["x21", 21],
-    ["x22", 22],
-    ["x23", 23],
-    ["x24", 24],
-    ["x25", 25],
-    ["x26", 26],
-    ["x27", 27],
-    ["x28", 28],
-    ["x29", 29],
-    ["x30", 30],
-    ["x31", 31],
-    ["pc", 32],
-    // ABI Register Names
-    ["zero", 0],
-    ["ra", 1],
-    ["sp", 2],
-    ["gp", 3],
-    ["tp", 4],
-    ["t0", 5],
-    ["t1", 6],
-    ["t2", 7],
-    ["fp", 8],
-    ["s0", 8],
-    ["s1", 9],
-    ["a0", 10],
-    ["a1", 11],
-    ["a2", 12],
-    ["a3", 13],
-    ["a4", 14],
-    ["a5", 15],
-    ["a6", 16],
-    ["a7", 17],
-    ["s2", 18],
-    ["s3", 19],
-    ["s4", 20],
-    ["s5", 21],
-    ["s6", 22],
-    ["s7", 23],
-    ["s8", 24],
-    ["s9", 25],
-    ["s10", 26],
-    ["s11", 27],
-    ["t3", 28],
-    ["t4", 29],
-    ["t5", 30],
-    ["t6", 31],
-    ["pc", 32],
-]);
-const INSTRUCTION_TO_FORMAT = new Map([
-    ["ADDI", I_TYPE],
-    ["SLTI", I_TYPE],
-    ["SLTIU", I_TYPE],
-    ["ANDI", I_TYPE],
-    ["ORI", I_TYPE],
-    ["XORI", I_TYPE],
-    ["SLLI", I_TYPE],
-    ["SRLI", I_TYPE],
-    ["SRAI", I_TYPE],
-    ["LUI", U_TYPE],
-    ["AUIPC", U_TYPE],
-    ["ADD", R_TYPE],
-    ["SUB", R_TYPE],
-    ["SLT", R_TYPE],
-    ["SLTU", R_TYPE],
-    ["SLL", R_TYPE],
-    ["SRL", R_TYPE],
-    ["NOP", NONE_TYPE],
-    ["MV", PSEUDO_TYPE],
-    ["SEQZ", PSEUDO_TYPE],
-    ["NOT", PSEUDO_TYPE],
-    ["JAL", J_TYPE],
-    ["JALR", J_TYPE],
-]);
 const I_INSTRUCTION_TO_FUNCTION = new Map([
     ["ADDI", addi],
     ["SLTI", slti],
@@ -139,11 +21,11 @@ const I_INSTRUCTION_TO_FUNCTION = new Map([
     ["XORI", xori],
     ["SLLI", slli],
     ["SRLI", srli],
-    ["SRAI", srai]
+    ["SRAI", srai],
 ]);
 const U_INSTRUCTION_TO_FUNCTION = new Map([
     ["LUI", lui],
-    ["AUIPC", auipc]
+    ["AUIPC", auipc],
 ]);
 const R_INSTRUCTION_TO_FUNCTION = new Map([
     ["ADD", add],
@@ -151,18 +33,22 @@ const R_INSTRUCTION_TO_FUNCTION = new Map([
     ["SLT", slt],
     ["SLTU", sltu],
     ["SLL", sll],
-    ["SRL", srl]
+    ["SRL", srl],
 ]);
 const NONE_INSTRUCTION_TO_FUNCTION = new Map([
-    ["NOP", () => { }]
+    ["NOP", () => { }],
 ]);
 /*** Functions ***/
 function updateRegisterDisplays() {
     registerDisplays.forEach((register_i, i) => {
-        const registerHex = registers.get(i).toString(16);
-        const zeros = "0".repeat(Math.max((8 - registerHex.length), 0));
+        const registerHex = registers.get(i).toString(16).slice(-7);
+        const zeros = "0".repeat(Math.max(8 - registerHex.length, 0));
         register_i.textContent = `0x${zeros}${registerHex}`;
     });
+}
+function setLeadingHexDigits(val, totalDigits) {
+    const zeros = "0".repeat(Math.max(totalDigits - val.toString(16).length, 0));
+    return `0x${zeros}${val.toString(16).slice(-totalDigits)}`;
 }
 function setRegister(rd, val) {
     /**
@@ -192,9 +78,15 @@ function addi(rd, rs1, imm) {
      * @param imm - The immediate value to add
      * @returns true if the operation was successful, false if trying to modify register x0
      */
-    console.log("Called addi function.");
-    const sourceRegister = STRINGS_TO_REGISTERS.get(rs1);
-    return setRegister(rd, Number(registers.get(sourceRegister)) + Number(imm));
+    if ((imm >= 4096) || (imm <= -4095)) {
+        console.log("Immediate value out of range");
+    }
+    /*
+    const sourceValue: string = STRINGS_TO_REGISTERS.get(rs1)!.toString(16).slice(-7);
+    const immValue: string = `${0.repeat()} ${imm.toString(16)} `;
+    return setRegister(rd, Number(registers.get(sourceRegister)!) + Number(imm));
+    */
+    return false;
 }
 function slti(rd, rs1, imm) {
     /**
@@ -209,12 +101,12 @@ function slti(rd, rs1, imm) {
      */
     console.log("Called slti function.");
     const sourceRegister = STRINGS_TO_REGISTERS.get(rs1);
-    console.log((Number(registers.get(sourceRegister)) < Number(imm)) ? 1 : 0);
-    return setRegister(rd, (Number(registers.get(sourceRegister)) < Number(imm)) ? 1 : 0);
+    return setRegister(rd, Number(registers.get(sourceRegister)) < Number(imm) ? 1 : 0);
 }
 function sltiu(rd, rs1, imm) {
     console.log("Called sltiu function.");
-    return false;
+    const sourceRegister = STRINGS_TO_REGISTERS.get(rs1);
+    return setRegister(rd, Number(registers.get(sourceRegister)) < Number(imm) ? 1 : 0);
 }
 function andi(rd, rs1, imm) {
     console.log("Called andi function.");
--- js/utils.ts
+++ js/utils.ts
@@ -6,142 +6,12 @@ Author: Zara Phukan.
 Creation Date: April 15, 2025.
 */
 
-// custom enum for stating instruction format
-enum OperandType {
-  IMMEDIATE,
-  REGISTER,
-}
-
-interface InstructionObject {
-  instruction: CallableFunction;
-  type: OperandType[];
-  operands: string[];
-}
-
 /*** Constants ***/
 const registerDisplays: HTMLParagraphElement[] = [];
 for (let i = 0; i < XLEN; i++) {
   registerDisplays.push(document.querySelector(`#register-${i}`)!);
 }
 registerDisplays.push(document.querySelector("#pc-register")!);
-
-const I_TYPE: OperandType[] = [
-  OperandType.REGISTER,
-  OperandType.REGISTER,
-  OperandType.IMMEDIATE,
-];
-const U_TYPE: OperandType[] = [OperandType.REGISTER, OperandType.IMMEDIATE];
-const R_TYPE: OperandType[] = [
-  OperandType.REGISTER,
-  OperandType.REGISTER,
-  OperandType.REGISTER,
-];
-const NONE_TYPE: OperandType[] = [];
-const PSEUDO_TYPE: OperandType[] = [OperandType.REGISTER, OperandType.REGISTER];
-const J_TYPE: OperandType[] = [];
-
-// for storing the current values in the registers
-const registers: Map<number, number> = new Map(
-  Array.from({ length: 33 }, (_, i) => [i, 0]),
-);
-
-const STRINGS_TO_REGISTERS: ReadonlyMap<string, number> = new Map([
-  // General Register Names
-  ["x0", 0],
-  ["x1", 1],
-  ["x2", 2],
-  ["x3", 3],
-  ["x4", 4],
-  ["x5", 5],
-  ["x6", 6],
-  ["x7", 7],
-  ["x8", 8],
-  ["x9", 9],
-  ["x10", 10],
-  ["x11", 11],
-  ["x12", 12],
-  ["x13", 13],
-  ["x14", 14],
-  ["x15", 15],
-  ["x16", 16],
-  ["x17", 17],
-  ["x18", 18],
-  ["x19", 19],
-  ["x20", 20],
-  ["x21", 21],
-  ["x22", 22],
-  ["x23", 23],
-  ["x24", 24],
-  ["x25", 25],
-  ["x26", 26],
-  ["x27", 27],
-  ["x28", 28],
-  ["x29", 29],
-  ["x30", 30],
-  ["x31", 31],
-  ["pc", 32],
-  // ABI Register Names
-  ["zero", 0],
-  ["ra", 1],
-  ["sp", 2],
-  ["gp", 3],
-  ["tp", 4],
-  ["t0", 5],
-  ["t1", 6],
-  ["t2", 7],
-  ["fp", 8],
-  ["s0", 8],
-  ["s1", 9],
-  ["a0", 10],
-  ["a1", 11],
-  ["a2", 12],
-  ["a3", 13],
-  ["a4", 14],
-  ["a5", 15],
-  ["a6", 16],
-  ["a7", 17],
-  ["s2", 18],
-  ["s3", 19],
-  ["s4", 20],
-  ["s5", 21],
-  ["s6", 22],
-  ["s7", 23],
-  ["s8", 24],
-  ["s9", 25],
-  ["s10", 26],
-  ["s11", 27],
-  ["t3", 28],
-  ["t4", 29],
-  ["t5", 30],
-  ["t6", 31],
-  ["pc", 32],
-]);
-
-const INSTRUCTION_TO_FORMAT: ReadonlyMap<string, OperandType[]> = new Map([
-  ["ADDI", I_TYPE],
-  ["SLTI", I_TYPE],
-  ["SLTIU", I_TYPE],
-  ["ANDI", I_TYPE],
-  ["ORI", I_TYPE],
-  ["XORI", I_TYPE],
-  ["SLLI", I_TYPE],
-  ["SRLI", I_TYPE],
-  ["SRAI", I_TYPE],
-  ["LUI", U_TYPE],
-  ["AUIPC", U_TYPE],
-  ["ADD", R_TYPE],
-  ["SUB", R_TYPE],
-  ["SLT", R_TYPE],
-  ["SLTU", R_TYPE],
-  ["SLL", R_TYPE],
-  ["SRL", R_TYPE],
-  ["NOP", NONE_TYPE],
-  ["MV", PSEUDO_TYPE],
-  ["SEQZ", PSEUDO_TYPE],
-  ["NOT", PSEUDO_TYPE],
-  ["JAL", J_TYPE],
-  ["JALR", J_TYPE],
-]);
 
 const I_INSTRUCTION_TO_FUNCTION: ReadonlyMap<string, Function> = new Map([
   ["ADDI", addi],
@@ -174,12 +44,14 @@ const NONE_INSTRUCTION_TO_FUNCTION: ReadonlyMap<string, Function> = new Map([
 ]);
 
 /*** Functions ***/
-function updateRegisterDisplays() {
-  registerDisplays.forEach((register_i, i) => {
-    const registerHex: string = registers.get(i)!.toString(16);
-    const zeros: string = "0".repeat(Math.max(8 - registerHex.length, 0));
-    register_i.textContent = `0x${zeros}${registerHex}`;
-  });
+function setNumHexDigits(val: number, totalDigits: number): string { 
+  const zeros: string = "0".repeat(Math.max(totalDigits - val.toString(16).length, 0));
+  return `0x${zeros}${val.toString(16).slice(-totalDigits)}`;
+}
+
+function setNumBinaryDigits(val: number, totalDigits: number): string { 
+  const zeros: string = "0".repeat(Math.max(totalDigits - val.toString(2).length, 0));
+  return `0b${zeros}${val.toString(2).slice(-totalDigits)}`;
 }
 
 function setRegister(rd: string, val: number): boolean {
@@ -193,7 +65,10 @@ function setRegister(rd: string, val: number): boolean {
   const register: number = STRINGS_TO_REGISTERS.get(rd)!;
   if (register != 0) {
     registers.set(register, val);
-    updateRegisterDisplays();
+    registerDisplays.forEach((register_i, i) => {
+      const registerHex: number = registers.get(i)!;
+      register_i.textContent = `0x${setNumHexDigits(registerHex, 8)}`;
+    });
     return true;
   } else {
     return false;
@@ -210,9 +85,17 @@ function addi(rd: string, rs1: string, imm: number): boolean {
    * @param imm - The immediate value to add
    * @returns true if the operation was successful, false if trying to modify register x0
    */
-  console.log("Called addi function.");
-  const sourceRegister: number = STRINGS_TO_REGISTERS.get(rs1)!;
+  if ((imm >= 4096) || (imm <= -4095)) { 
+    console.log("Immediate value out of range. It will be truncated to its lower 12 bits.");
+    return false;
+  }
+  const sourceValue: string = setNumHexDigits(, totalDigits)
+  /*
+  const sourceValue: string = STRINGS_TO_REGISTERS.get(rs1)!.toString(16).slice(-7);
+  const immValue: string = `${0.repeat()} ${imm.toString(16)} `; 
   return setRegister(rd, Number(registers.get(sourceRegister)!) + Number(imm));
+  */
+  return false;
 }
 
 function slti(rd: string, rs1: string, imm: number): boolean {
