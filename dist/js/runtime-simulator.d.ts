dc85dc3fc41bab65fd8dac376198b67e3fb9d32e
ZaraPhu <103774818+ZaraPhu@users.noreply.github.com>
2025-04-27 16:58:44 -0300

    Delete dist directory
    Removing dist from existing remote repo

--- dist/js/runtime-simulator.d.ts
+++
@@ -1,6 +0,0 @@
-declare const assemblyEditor: HTMLTextAreaElement | null;
-declare const assembleButton: HTMLButtonElement | null;
-declare const errorText: HTMLParagraphElement | null;
-declare function raiseError(message: string): void;
-declare function parseInput(instructionList: string[]): ParserResult;
-declare function executeInstruction(destructuredInstruction: string[]): boolean;
