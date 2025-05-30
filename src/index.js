fd296959eef2fed17e6a734b31e903d6f8d2cffe
ZaraPhu <zara.phukan@gmail.com>
2025-05-30 12:21:50 -0300

    Removed server side index.js file and wrangler config. Uninstalled wrangler from node_modules. Not needed.

--- src/index.js
+++
@@ -1,6 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    // This will automatically use files from your site assets (public directory)
-    return env.ASSETS.fetch(request);
-  }
-};
